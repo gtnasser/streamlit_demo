9cafd0f5a6ce304e6e71af58163a5f36e8bd1c37
Giba Nasser <gtnasser@gmail.com>
2024-11-15 15:48:01 -0300

    docs: readme & main page info

---
+++ README.md
@@ -0,0 +1,10 @@
+# streamlit_demo
+
+Repositório do app feito usando o framework [Streamlit](https://streamlit.io/).
+
+The main goal is learning Streamlit Framework, but, I´ll fill this app with several sample pages.
+
+
+### Features
+
+* multipage
--- app.py
+++ app.py
@@ -2,8 +2,20 @@ import streamlit as st
 
 st.session_state.Appcount = 1 + (st.session_state.get('Appcount') or 0)
 
-st.header('app.py - Main Page')
+st.header('Demo Streamlit App')
 
-st.subheader('Page content here')
+#st.write(':red[session_state]:',st.session_state)
 
-st.write(':red[session_state]:',st.session_state)
+st.markdown('''
+This is my project powered by [Streamlit :streamlit:](https://streamlit.io/).
+
+The main goal is FUN, despite the learning of Streamlit Framework.
+
+Full repo project in my [Github](https://github.com/gtnasser/streamlit_demo.git)
+
+
+### Features
+
+* multipage
+
+''')
